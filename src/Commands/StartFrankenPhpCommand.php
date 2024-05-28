9da9b4815b7815615978f82d676e5b99ccae529c
Alex Cantu <miguel.can2@gmail.com>
2024-05-28 08:38:01 -0500

    Set admin-host option default to `localhost`, remove adminHost function

--- src/Commands/StartFrankenPhpCommand.php
+++ src/Commands/StartFrankenPhpCommand.php
@@ -28,7 +28,7 @@ class StartFrankenPhpCommand extends Command implements SignalableCommandInterfa
                     {--host=127.0.0.1 : The IP address the server should bind to}
                     {--port= : The port the server should be available on}
                     {--admin-port= : The port the admin server should be available on}
-                    {--admin-host= : The host the admin server should be availble on}
+                    {--admin-host=localhost : The host the admin server should be availble on}
                     {--workers=auto : The number of workers that should be available to handle requests}
                     {--max-requests=500 : The number of requests to process before reloading the server}
                     {--caddyfile= : The path to the FrankenPHP Caddyfile file}
@@ -98,7 +98,7 @@ class StartFrankenPhpCommand extends Command implements SignalableCommandInterfa
             'REQUEST_MAX_EXECUTION_TIME' => $this->maxExecutionTime(),
             'CADDY_GLOBAL_OPTIONS' => ($https && $this->option('http-redirect')) ? '' : 'auto_https disable_redirects',
             'CADDY_SERVER_ADMIN_PORT' => $this->adminPort(),
-            'CADDY_SERVER_ADMIN_HOST' => $this->adminHost(),
+            'CADDY_SERVER_ADMIN_HOST' => $this->option('admin-host'),
             'CADDY_SERVER_LOG_LEVEL' => $this->option('log-level') ?: (app()->environment('local') ? 'INFO' : 'WARN'),
             'CADDY_SERVER_LOGGER' => 'json',
             'CADDY_SERVER_SERVER_NAME' => $serverName,
@@ -212,7 +212,7 @@ class StartFrankenPhpCommand extends Command implements SignalableCommandInterfa
             'host' => $this->getHost(),
             'port' => $this->getPort(),
             'adminPort' => $this->adminPort(),
-            'adminHost' => $this->adminHost(),
+            'adminHost' => $this->option('admin-host'),
             'workers' => $this->workerCount(),
             'maxRequests' => $this->option('max-requests'),
             'octaneConfig' => config('octane'),
@@ -239,20 +239,6 @@ class StartFrankenPhpCommand extends Command implements SignalableCommandInterfa
                 );
             }
         });
-    }
-
-    /**
-     * Get the host the admin API should be available on.
-     *
-     * @return string
-     */
-    protected function adminHost()
-    {
-        if ($this->option('admin-host')) {
-            return (string) $this->option('admin-host');
-        }
-
-        return 'localhost';
     }
 
     /**
